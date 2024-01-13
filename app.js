4a88f119de83de151fdeb01078ffa39f50389dee
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2024-01-13 19:11:32 +0100

    Hello World RabbitMQ

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ const Challenge = require('./routes/challenge');
 const AllChallenges = require('./routes/allChallenges');
 const CreboMan = require('./routes/CreboMan');
 
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'hello';
+        var msg = 'Hello world';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+});
+
+exports.amqp = amqp;
+
 const app = express();
 
 app.use(logger('dev'));
